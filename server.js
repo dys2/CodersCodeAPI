35e5e35fab7c40e503d32bfbe7dd1417b2547618
Dylan <dylan@Dylans-MacBook.local>
2017-10-20 21:05:37 -0400

    up

--- server.js
+++ server.js
@@ -10,18 +10,20 @@ server.use(bodyParser.json({limit: '50mb'}));
 server.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 server.use(cors());
 
+
+console.log(process.env);
 const port = process.env.PORT || 5000
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
+mongoose.connect('mongodb://heroku_xk76h9r4:54m64u1qs8d89jvud60ue4a5mg@ds125565.mlab.com:25565/heroku_xk76h9r4', { useMongoClient: false });
 
 const db = mongoose.connection;
 	db.on('error', console.error.bind(console, 'database connection error...'));
   db.once('open', () => console.log('Db connection active...'));
   
 
-const routes = require('./api/routes/routes');
+const routes = require('./routes/routes');
 routes(server);
 
 server.listen(port, () => {
